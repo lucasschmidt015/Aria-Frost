4f5f4850c62b4ac3dbb81f97c3afe2cb6472e892
Lucas Schmidt <lucas.schmidt015@gmail.com>
2023-11-16 20:18:37 -0300

    A simple label adjust. I added route protection to the about page.

--- routes/simplePages.js
+++ routes/simplePages.js
@@ -2,8 +2,11 @@ const express = require("express");
 
 const simplePagesController = require("../controllers/simplePages");
 
+const isAuth = require("../middlewares/isAuth");
+const isAccountValid = require("../middlewares/isAccountValid");
+
 const router = express.Router();
 
-router.get("/about", simplePagesController.getAbout);
+router.get("/about", isAuth, isAccountValid, simplePagesController.getAbout);
 
 module.exports = router;
